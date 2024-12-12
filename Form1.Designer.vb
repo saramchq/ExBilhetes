fba15df3d0e02b2949c561388ba0e0a9867b0513
Sara <Sara@DESKTOP-R2BTG91>
2024-12-12 22:21:51 +0000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,289 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CmbPartida = New System.Windows.Forms.ComboBox()
+        Me.CmbDestino = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnConfirmar = New System.Windows.Forms.Button()
+        Me.btnLimpar = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.txtPartida = New System.Windows.Forms.TextBox()
+        Me.txtDestino = New System.Windows.Forms.TextBox()
+        Me.txtPreco = New System.Windows.Forms.TextBox()
+        Me.txtNumBilhetes = New System.Windows.Forms.TextBox()
+        Me.txtValorPago = New System.Windows.Forms.TextBox()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.txtTroco = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(306, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(248, 26)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Comboios de Portugal"
+        '
+        'CmbPartida
+        '
+        Me.CmbPartida.FormattingEnabled = True
+        Me.CmbPartida.Location = New System.Drawing.Point(81, 227)
+        Me.CmbPartida.Name = "CmbPartida"
+        Me.CmbPartida.Size = New System.Drawing.Size(173, 21)
+        Me.CmbPartida.TabIndex = 2
+        '
+        'CmbDestino
+        '
+        Me.CmbDestino.FormattingEnabled = True
+        Me.CmbDestino.Location = New System.Drawing.Point(81, 353)
+        Me.CmbDestino.Name = "CmbDestino"
+        Me.CmbDestino.Size = New System.Drawing.Size(173, 21)
+        Me.CmbDestino.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(81, 192)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 17)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Partida:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(81, 311)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 17)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Destino:"
+        '
+        'btnConfirmar
+        '
+        Me.btnConfirmar.Location = New System.Drawing.Point(81, 463)
+        Me.btnConfirmar.Name = "btnConfirmar"
+        Me.btnConfirmar.Size = New System.Drawing.Size(94, 25)
+        Me.btnConfirmar.TabIndex = 6
+        Me.btnConfirmar.Text = "Confirmar"
+        Me.btnConfirmar.UseVisualStyleBackColor = True
+        '
+        'btnLimpar
+        '
+        Me.btnLimpar.Location = New System.Drawing.Point(224, 463)
+        Me.btnLimpar.Name = "btnLimpar"
+        Me.btnLimpar.Size = New System.Drawing.Size(94, 25)
+        Me.btnLimpar.TabIndex = 7
+        Me.btnLimpar.Text = "Limpar"
+        Me.btnLimpar.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(557, 160)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(178, 17)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Dados para Pagamento"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(483, 229)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(53, 17)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Partida"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(483, 278)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(56, 17)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Destino"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(483, 332)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(45, 17)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "Preço"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(483, 385)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(77, 17)
+        Me.Label8.TabIndex = 12
+        Me.Label8.Text = "Nº Bilhetes"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(483, 441)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(78, 17)
+        Me.Label9.TabIndex = 13
+        Me.Label9.Text = "Valor Pago"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(483, 563)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(45, 17)
+        Me.Label10.TabIndex = 14
+        Me.Label10.Text = "Troco"
+        '
+        'txtPartida
+        '
+        Me.txtPartida.Location = New System.Drawing.Point(577, 229)
+        Me.txtPartida.Name = "txtPartida"
+        Me.txtPartida.Size = New System.Drawing.Size(158, 20)
+        Me.txtPartida.TabIndex = 15
+        '
+        'txtDestino
+        '
+        Me.txtDestino.Location = New System.Drawing.Point(577, 278)
+        Me.txtDestino.Name = "txtDestino"
+        Me.txtDestino.Size = New System.Drawing.Size(158, 20)
+        Me.txtDestino.TabIndex = 16
+        '
+        'txtPreco
+        '
+        Me.txtPreco.Location = New System.Drawing.Point(577, 332)
+        Me.txtPreco.Name = "txtPreco"
+        Me.txtPreco.Size = New System.Drawing.Size(158, 20)
+        Me.txtPreco.TabIndex = 17
+        '
+        'txtNumBilhetes
+        '
+        Me.txtNumBilhetes.Location = New System.Drawing.Point(577, 385)
+        Me.txtNumBilhetes.Name = "txtNumBilhetes"
+        Me.txtNumBilhetes.Size = New System.Drawing.Size(158, 20)
+        Me.txtNumBilhetes.TabIndex = 18
+        '
+        'txtValorPago
+        '
+        Me.txtValorPago.Location = New System.Drawing.Point(577, 444)
+        Me.txtValorPago.Name = "txtValorPago"
+        Me.txtValorPago.Size = New System.Drawing.Size(158, 20)
+        Me.txtValorPago.TabIndex = 19
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(621, 493)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 21
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'txtTroco
+        '
+        Me.txtTroco.Location = New System.Drawing.Point(577, 563)
+        Me.txtTroco.Name = "txtTroco"
+        Me.txtTroco.Size = New System.Drawing.Size(158, 20)
+        Me.txtTroco.TabIndex = 20
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gainsboro
+        Me.ClientSize = New System.Drawing.Size(838, 687)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.txtTroco)
+        Me.Controls.Add(Me.txtValorPago)
+        Me.Controls.Add(Me.txtNumBilhetes)
+        Me.Controls.Add(Me.txtPreco)
+        Me.Controls.Add(Me.txtDestino)
+        Me.Controls.Add(Me.txtPartida)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnLimpar)
+        Me.Controls.Add(Me.btnConfirmar)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.CmbDestino)
+        Me.Controls.Add(Me.CmbPartida)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CmbPartida As ComboBox
+    Friend WithEvents CmbDestino As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnConfirmar As Button
+    Friend WithEvents btnLimpar As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents txtPartida As TextBox
+    Friend WithEvents txtDestino As TextBox
+    Friend WithEvents txtPreco As TextBox
+    Friend WithEvents txtNumBilhetes As TextBox
+    Friend WithEvents txtValorPago As TextBox
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents txtTroco As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,78 @@
+﻿Public Class Form1
+    ' Variáveis globais para armazenar o preço por bilhete
+    Dim precoBilhete As Double = 15.0 ' Preço padrão de 15 euros
+
+    ' Evento de clique do botão Confirmar
+    Private Sub btnConfirmar_Click(sender As Object, e As EventArgs) Handles btnConfirmar.Click
+        ' Exibe os valores selecionados nas caixas de texto correspondentes
+        txtPartida.Text = cmbPartida.SelectedItem.ToString()
+        txtDestino.Text = cmbDestino.SelectedItem.ToString()
+        txtPreco.Text = precoBilhete.ToString("F2")
+    End Sub
+
+    ' Evento de clique do botão Calcular
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        ' Verifica se o número de bilhetes e o valor pago estão preenchidos corretamente
+        If txtNumBilhetes.Text <> "" And txtValorPago.Text <> "" Then
+            Dim numBilhetes As Integer = Integer.Parse(txtNumBilhetes.Text)
+            Dim valorPago As Double = Double.Parse(txtValorPago.Text)
+            Dim total As Double = numBilhetes * precoBilhete
+            Dim troco As Double = valorPago - total
+
+            ' Exibe o troco
+            txtTroco.Text = troco.ToString("F2")
+        Else
+            MessageBox.Show("Por favor, insira o número de bilhetes e o valor pago.")
+        End If
+    End Sub
+
+    ' Evento de clique do botão Limpar
+    Private Sub btnLimpar_Click(sender As Object, e As EventArgs) Handles btnLimpar.Click
+        ' Limpa todos os campos
+        cmbPartida.SelectedIndex = -1
+        cmbDestino.SelectedIndex = -1
+        txtPartida.Clear()
+        txtDestino.Clear()
+        txtPreco.Clear()
+        txtNumBilhetes.Clear()
+        txtValorPago.Clear()
+        txtTroco.Clear()
+    End Sub
+
+    ' Evento de inicialização do formulário
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Adiciona os valores aos ComboBox
+        CmbPartida.Items.Add("Lisboa")
+        cmbPartida.Items.Add("Porto")
+        cmbDestino.Items.Add("Lisboa")
+        cmbDestino.Items.Add("Porto")
+
+        ' Define os valores padrão
+        cmbPartida.SelectedIndex = 0 ' Lisboa como padrão
+        cmbDestino.SelectedIndex = 1 ' Porto como padrão
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+
+    End Sub
+
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles txtValorPago.TextChanged
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+
+    End Sub
+
+    Private Sub Label9_Click(sender As Object, e As EventArgs) Handles Label9.Click
+
+    End Sub
+End Class
